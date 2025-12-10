da220d553954660ba957b86cfadc38a2b0824766
Rob Bierman <rbierman@princeton.edu>
2025-12-10 13:25:19 -0500

    wip: Day10 p2 rust naive far too slow

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 use std::collections::{VecDeque, HashSet};
 
-fn parse_comma_sep_nums(token: &str) -> Vec<u32> {
+fn parse_comma_sep_nums(token: &str) -> Vec<usize> {
     let mut token_iter = token.chars();
     token_iter.next(); //remove start
     token_iter.next_back(); // remove end
@@ -10,7 +10,7 @@ fn parse_comma_sep_nums(token: &str) -> Vec<u32> {
     stripped_t.split(',').filter_map(|c| c.parse().ok()).collect()
 }
 
-fn parse_line(l: &str) -> (u32, Vec<u32>, Vec<u32>) {
+fn parse_line(l: &str) -> (usize, Vec<usize>, Vec<Vec<usize>>, Vec<usize>) {
 
     let mut tokens = l.trim().split(' ');
     let light_token = tokens.next().unwrap();
@@ -21,35 +21,38 @@ fn parse_line(l: &str) -> (u32, Vec<u32>, Vec<u32>) {
                                      .filter(|c| *c == '.' || *c == '#')
                                      .map(|c| if c == '.' {'0'} else {'1'}).collect();
 
-    let lights = u32::from_str_radix(&s_lights, 2).expect("Not a binary number!");
+    let lights = usize::from_str_radix(&s_lights, 2).expect("Not a binary number!");
 
     // Parse the joltages
     let jolts = parse_comma_sep_nums(jolts_token);
 
     // Parse the buttons
-    let mut buttons: Vec<u32> = vec![];
-    let num_lights: u32 = s_lights.len().try_into().unwrap();
+    let mut buttons: Vec<Vec<usize>> = vec![];
+    let mut bit_buttons: Vec<usize> = vec![];
+    let num_lights: usize = s_lights.len().try_into().unwrap();
     for token in tokens {
         let toggled_light_idxs = parse_comma_sep_nums(token);
-        let toggled:u32 = toggled_light_idxs.iter()
-                                        .map(|idx| 2_u32.pow(num_lights.abs_diff(*idx+1)))
-                                        .sum::<u32>();
 
-        buttons.push(toggled);
+        let bit_toggled:usize = toggled_light_idxs.iter()
+                                        .map(|idx| 2_usize.pow(num_lights.abs_diff(*idx+1).try_into().unwrap()))
+                                        .sum::<usize>();
+
+        buttons.push(toggled_light_idxs);
+        bit_buttons.push(bit_toggled);
     }
 
-    (lights, buttons, jolts)
+    (lights, bit_buttons, buttons, jolts)
 }
 
-pub fn part1(data_path: &Path) -> u32 {
+pub fn part1(data_path: &Path) -> usize {
     let text = std::fs::read_to_string(data_path).unwrap();
     let mut score = 0;
 
     for line in text.lines() {
-        let (target_state, buttons, _jolts) = parse_line(line);
+        let (target_state, buttons, _vec_buttons, _jolts) = parse_line(line);
 
-        let mut queue: VecDeque<(u32, u32)> = VecDeque::from([(0, 0)]);
-        let mut prior_states: HashSet<u32> = HashSet::new();
+        let mut queue: VecDeque<(usize, usize)> = VecDeque::from([(0, 0)]);
+        let mut prior_states: HashSet<usize> = HashSet::new();
 
         while let Some((current_state, num_presses)) = queue.pop_back() {
             if current_state == target_state {
@@ -59,8 +62,55 @@ pub fn part1(data_path: &Path) -> u32 {
                 for button in &buttons {
                     let new_state = current_state^button;
                     if ! prior_states.contains(&new_state){
-                        queue.push_front((current_state^button, num_presses+1));
+                        queue.push_front((new_state, num_presses+1));
                         prior_states.insert(new_state);
+                    }
+                }
+            }
+        }
+    }
+    score
+}
+
+
+pub fn part2(data_path: &Path) -> usize {
+    let text = std::fs::read_to_string(data_path).unwrap();
+    let mut score = 0;
+
+    for line in text.lines() {
+        println!("Working on {}", line);
+        let (_target_state, _bit_buttons, buttons, target_jolts) = parse_line(line);
+
+        let mut queue: VecDeque<(Vec<usize>, usize)> = VecDeque::new();
+        queue.push_front((vec![0; target_jolts.len()], 0));
+
+        let mut prior_states: HashSet<Vec<usize>> = HashSet::new();
+
+        while let Some((current_jolts, num_presses)) = queue.pop_back() {
+            println!("{:?} vs {:?}", current_jolts, target_jolts);
+            if current_jolts == target_jolts {
+                score += num_presses;
+                break;
+            } else {
+                for button in &buttons {
+                    let mut too_large = false;
+                    let mut new_jolts = vec![];
+                    for (i,current_jolt) in current_jolts.iter().enumerate() {
+                        if button.contains(&i) {
+                            let new_jolt = current_jolt+1;
+                            if new_jolt > target_jolts[i] {
+                                too_large = true;
+                                break
+                            }
+                            new_jolts.push(new_jolt);
+                        } else {
+                            new_jolts.push(*current_jolt);
+                        }
+                    }
+
+                    if !too_large && !prior_states.contains(&new_jolts){
+                        queue.push_front((new_jolts.clone(), num_presses+1));
+                        prior_states.insert(new_jolts);
                     }
                 }
             }
@@ -93,10 +143,36 @@ mod tests {
         (temp_dir, temp_file, f_path)
     }
 
+    fn custom_create_test_file() -> (tempfile::TempDir, File, PathBuf) {
+        let test_input = "\
+[..#.##] (0,1,3,4) (0,3,4) (0,5) (0,1,2) (3,5) (0,2,3,4) (2,3) {58,27,37,57,37,24}";
+        let temp_dir = tempdir().unwrap();
+        let f_path = temp_dir.path().join("test_input.txt");
+        let mut temp_file = File::create(f_path.clone()).unwrap();
+        write!(temp_file, "{}", test_input).unwrap();
+
+        // have to return dir and file so they don't go out of scope
+        (temp_dir, temp_file, f_path)
+    }
+
     #[test]
     fn test_part1() {
         let (_d, _f, test_path) = create_test_file();
         let result = part1(&test_path);
         assert_eq!(result, 7);
+    }
+
+    //#[test]
+    //fn test_part2() {
+    //    let (_d, _f, test_path) = create_test_file();
+    //    let result = part2(&test_path);
+    //    assert_eq!(result, 33);
+    //}
+
+    #[test]
+    fn test_part2_custom() {
+        let (_d, _f, test_path) = custom_create_test_file();
+        let result = part2(&test_path);
+        assert_eq!(result, 33);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -94,8 +94,13 @@ fn day9_part2(data_path: &str) -> PyResult<u64> {
 }
 
 #[pyfunction]
-fn day10_part1(data_path: &str) -> PyResult<u32> {
+fn day10_part1(data_path: &str) -> PyResult<usize> {
     Ok(days::day10::part1(Path::new(data_path)))
+}
+
+#[pyfunction]
+fn day10_part2(data_path: &str) -> PyResult<usize> {
+    Ok(days::day10::part2(Path::new(data_path)))
 }
 
 #[pymodule]
@@ -119,5 +124,6 @@ fn advent2025(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(day9_part1, m)?)?;
     m.add_function(wrap_pyfunction!(day9_part2, m)?)?;
     m.add_function(wrap_pyfunction!(day10_part1, m)?)?;
+    m.add_function(wrap_pyfunction!(day10_part2, m)?)?;
     Ok(())
 }
